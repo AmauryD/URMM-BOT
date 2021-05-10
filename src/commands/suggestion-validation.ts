d90740ad8e0396e7234634bcb4d27832883540ee
DramixDW <verliefden.romain@gmail.com>
2021-05-10 17:51:28 +0200

    Impletement reaction answer for suggestion-validation
    As short as it is to type 'y' or 'no', it seems like a burden
    when you have to type it a lot of times. Changing for a way to
    answer the bot through reaction so that it become more user-friendly
    as well as maximing the discord possibilities.

--- src/commands/suggestion-validation.ts
+++ src/commands/suggestion-validation.ts
@@ -29,6 +29,8 @@ export const action: CommandAction = async function (
     .getMany();
 
   await originalMessage.reply(`Il y a ${propositions.length} propositions en attente.`);
+  const validationEmoji = '✅';
+  const denyEmoji = '❌';
 
     for (let prop of propositions) {
 
@@ -37,17 +39,31 @@ export const action: CommandAction = async function (
         originalMessage
       );
 
-      if (isValidated === "y") {
-        prop.state = PropositionState.VALIDATED;
-      } else {
-        const denyingReason = await askQuestion(
-          `Pourquoi ${prop.name} est une suggestion invalide ?`,
-          originalMessage
-        );
-        prop.state = PropositionState.DENIED;
-        prop.note = `Raison du refus : ${denyingReason}`;
-      }
+      originalMessage.react(validationEmoji)
+      .then(() => originalMessage.react(denyEmoji));
 
+      const filter = (reaction) => {
+        return ['✅', '❌'].includes(reaction.emoji.name);
+      };
+
+      originalMessage.awaitReactions(filter, { max: 1, time: 5000, errors: ['time'] })
+      .then(async collected => {
+        const reaction = collected.first();
+
+        if (reaction!.emoji.name === '✅') {
+          prop.state = PropositionState.VALIDATED;
+        } else {
+          const denyingReason = await askQuestion(
+            `Pourquoi ${prop.name} est une suggestion invalide ?`,
+            originalMessage
+          );
+          prop.state = PropositionState.DENIED;
+          prop.note = `${denyingReason}`;
+        }
+      })
+      .catch(collected => {
+        originalMessage.reply('Ce n\'est pas une réaction valide :c');
+      });
     }
 
   await propositionRepo.save(propositions);
