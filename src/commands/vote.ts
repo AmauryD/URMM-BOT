8b44b1ebea9959defcf0f99b2e3e832aa6588bf4
DramixDW <verliefden.romain@gmail.com>
2021-05-14 02:23:10 +0200

    Adding Vote through Reaction
    Let the user vote through reaction so that he doesn't have to type a
    command. also, avoid confusion with numbers

--- src/commands/vote.ts
+++ src/commands/vote.ts
@@ -1,11 +1,11 @@
 import { MessageEmbed } from "discord.js";
 import stc from "string-to-color";
-import { getCustomRepository, getRepository } from "typeorm";
+import { Any, getCustomRepository, getRepository } from "typeorm";
 import { CommandAction, CommandHandler } from "../commandHandler";
 import { TourType } from "../models/tour";
 import { Vote } from "../models/vote";
+import { VoteProposition } from "../models/vote-proposition";
 import { TourRepository } from "../repositories/tour.repository";
-import { askQuestion } from "../utils/ask-question";
 import getCurrentPoll from "../utils/get-current-poll";
 
 export const commandName = "vote";
@@ -38,7 +38,6 @@ export const action: CommandAction = async function (
     throw new Error("Aucun tour n'a encore été posté :(");
   }
 
-
   const hasVoted = await repo
     .createQueryBuilder("tour")
     .leftJoinAndSelect("tour.votePropositions", "votePropositions")
@@ -51,70 +50,54 @@ export const action: CommandAction = async function (
     throw new Error("Vous avez déjà voté :(");
   }
 
-  if (lastTour.type === TourType.Single) {
-    const embed = new MessageEmbed()
-      .setColor(stc(currentPoll.name))
-      .setTitle("💬 Votez !")
-      .setDescription("Choisissez le chiffre correspondant à la proposition que vous voulez choisir !")
-      .addField(`⬇ Propositions`,lastTour.votePropositions.map(
-        (e, i) => `\t🔹 ${i} : ${e.proposition.name}`
-      ).join("\n"))
-      .addField("❔ Explications","Veuillez faire votre choix (ex : `1`) !\nCe tour est à réponse unique !");
+  const embed = new MessageEmbed()
+    .setColor(stc(currentPoll.name))
+    .setTitle("💬 Votez !")
+  if(lastTour.type === TourType.Multiple) embed.setDescription("Réagissez avec 📜 sur les suggestions de votre choix"); 
+  else embed.setDescription("Réagissez avec 📜 sur la suggestion de votre choix (vous ne pouvez en choisir qu'une)");
+  originalMessage.reply(embed);
 
-    const response = await askQuestion(
-      embed,
-      originalMessage,
-      120000
-    );
+  const propositions = [];  
+  const votePropositionsIDs: { [name :string] : VoteProposition} = {};
+  for (const voteProp of lastTour.votePropositions) {
+      votePropositionsIDs[voteProp.proposition.name] = voteProp;
+      const prop = await originalMessage.reply(voteProp.proposition.name);
+      propositions.push(prop);
+      await prop.react('📜');
+  }
 
-    const voteProposition = lastTour.votePropositions[parseInt(response, 10)];
-    const vote = voteRepo.create({
-      voteProposition,
-      clientId: originalMessage.author!.id,
-    });
-    await voteRepo.save(vote);
-  }else{
-    const indexes = lastTour.votePropositions.map((e,i) => i);
+  const confirmEmbed = new MessageEmbed()
+  .setColor(stc(currentPoll.name))
+  .setTitle('Urne 🗳')
+  .setDescription("Réagissez sur ce message avec ✅ une fois que vous avez fait votre choix :)");
 
-    const embed = new MessageEmbed()
-      .setColor(stc(currentPoll.name))
-      .setTitle("💬 Votez !")
-      .setDescription("Choisissez les chiffres correspondants aux propositions que vous voulez choisir !")
-      .addField(`⬇ Propositions`,lastTour.votePropositions.map(
-        (e, i) => `\t🔹 ${i} : ${e.proposition.name}`
-      ).join("\n"))
-      .addField("❔ Explications","Veuillez faire votre choix (ex : `1`) !\nSi plusieurs choix séparer par une virgule comme ceci : (ex : `1,2,3,4`)");
-
-    const response = await askQuestion(
-      embed,
-      originalMessage,
-      120000
-    );
-
-    const chosen = response.split(",").map(r => r.trim()).filter((e) => e !== "");
-
-    if (chosen.length === 0) {
-      throw new Error("Vous devez proposer quelque chose !\nVeuillez revoter avec la commande `$vote`");
-    }
-
-    if (chosen.some((e) => !indexes.includes(parseInt(e,10)))) {
-      throw new Error("Vous devez choisir une proposition valide !\nVeuillez revoter avec la commande `$vote`");
-    }
-
-    for (const code of chosen) {
-      const voteProposition = lastTour.votePropositions[parseInt(code, 10)];
+  const confirm = await originalMessage.reply(confirmEmbed);
+  await confirm.react('✅');
+  //10 Minutes should be enough to choose and vote, i hope :/
+  //Also, not bothering checking if user is the one who did $vote because he should be the only one able to react as it is the only other one in the channel
+  await confirm.awaitReactions( (react,user) => react.emoji.name === '✅', { max: 1, time: 600000, errors: ['time'] });
+  const chosen = [];
+  const votes = [];
+  for (const prop of propositions) {
+    // Only checking count because as there is only 2 user in the channel (bot and user), if there's 2 reactions, the user must be the one who added the second reaction.
+    if (prop.reactions.cache.get('📜')!.count === 2){
+      const voteProposition = votePropositionsIDs[prop.content];
+      chosen.push(prop.content);
       const vote = voteRepo.create({
         voteProposition,
         clientId: originalMessage.author.id,
       });
-      await voteRepo.save(vote);
+      votes.push(vote);
     }
+    if (chosen.length > 1 && lastTour.type === TourType.Single) throw new Error("Vous ne pouviez voter que pour une suggestion :c\n Envoyez moi $vote pour recommencer :wink:");
   }
+  //Saving after the loop so that a vote is not registered if there's multiple vote in a single type tour 
+  await voteRepo.save(votes);
 
-  const embed = new MessageEmbed()
+  const voteAck = new MessageEmbed()
       .setColor(stc(currentPoll.name))
-      .setTitle("✅ Votre vote a été comptabilisé !")
+      .setTitle(`✅ Votre vote pour [${chosen.join()}] a été comptabilisé !`)
       .setDescription("Utilisez la commande `$status` afin de voir les différents résultats.");
 
-  await originalMessage.reply(embed);
+  await originalMessage.reply(voteAck);
 };
